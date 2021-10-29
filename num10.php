4e745a15a44d926258acc79d0dc3d24d7810d51f
したけ <92673467+sasakitakeshi613@users.noreply.github.com>
2021-10-29 11:36:50 +0900

    Update num10.php

--- num10.php
+++ num10.php
@@ -2,13 +2,19 @@
 	require_once("tool/config.php");
 	
 	$num1 = [];
-	for($i = 0; $i < 10; $i++){
+	
+	if($_GET["d_id"] == 4){
+		$sql = "SELECT * FROM englishwordtable ORDER BY rand() LIMIT 1";
+		$rs = $pdo->query($sql);
+		$num1 = $rs->fetch(PDO::FETCH_ASSOC);
+		echo json_encode($num1);
+	} else {
 		$sql = "SELECT * FROM englishwordtable"; 
 		$sql .= " WHERE d_id=:d_id ORDER BY rand() LIMIT 1";
 		$stmt = $pdo->prepare($sql);
-		$stmt->bindValue(":d_id", $_GET["d_id"], PDO::PARAM_INT);
+		$stmt->bindValue(":d_id", $_GET['d_id'], PDO::PARAM_INT);
 		$stmt->execute();
-		$num1[] = $stmt->fetch(PDO::FETCH_ASSOC);
+		$num1 = $stmt->fetch(PDO::FETCH_ASSOC);
+		echo json_encode($num1);
 	}
-	echo json_encode($num1);
 ?>
